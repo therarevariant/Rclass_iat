18350b519b222a5ed331855971dc53f9fa7d141f
therarevariant <therarevariant@gmail.com>
2014-05-20 05:12:43 +0000

    Initial commit of mod12 file from class

---
+++ Mod12.R
@@ -0,0 +1,30 @@
+GitHub Notes
+
+https://www.rstudio.com/ide/docs/version_control/overview
+http://www.molecularecologist.com/2013/11/using-github-with-r-and-rstudio/  
+http://nyuccl.org/pages/GitTutorial/  
+http://learndataanalysis.com/easy-access-github-rstudio
+
+HTTP vs. SSH
+Unless you use GitHub a lot, use a push on HTTP
+SSH easier on Macs and Windows
+
+Sync with GitHub:
+  New Project
+  Version Control
+  Git
+  Repository URL = git address
+
+A GIT tab will appear next to environment and history
+Make changes, 
+Stage Changes
+Committ locally
+Push online
+
+Click "Commit" to push, pull, or commit
+In file list:
+  M = modified
+  D = deleted
+  ? = unknown status
+
+Push - pushes it to the server (and removes it from the GIT tab)
